2b663e8864428daa2e1db6b9b2e1b3079cb10ee8
BugHunter-X <askzenai@gmail.com>
2025-01-26 06:02:41 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.writeHead(200);
+  response.end(JSON.stringify({ message: 'Hello from Node.js!' }));
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
+
+//Uncommon bug:  The server might fail silently if port 8080 is already in use.  Standard error handling doesn't always catch this. 
